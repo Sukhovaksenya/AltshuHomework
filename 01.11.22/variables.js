e11c9d311b2f22209a7921edfa4f285402dc4202
Sukhovaksenya <117826749+Sukhovaksenya@users.noreply.github.com>
2022-11-09 15:12:53 +0300

    Add files via upload

---
+++ 01.11.22/variables.js
@@ -0,0 +1,15 @@
+let myAge = 32;
+let ageMyBestFriend = 33;
+let myBirthYear = 1990;
+let currentYear = 2022;
+let rublesPerDollar = 61.43;
+
+const myName = 'Ksenya';
+const myBestFriendName = 'Regina';
+const myCity = 'Saint-Petersburg';
+const capitalOfMyCountry ='Moscow';
+const myDesctipionInOneWord = 'Jewish';
+
+console.log (`My name is ${myName}, I am ${myAge} years old, I am from ${myCity}, I am ${myDesctipionInOneWord}.`); 
+
+
