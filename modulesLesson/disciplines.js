d7cb6ba64206310c1682fbae7c2757cfa50eb817
Ksenya Sukhova <117826749+Sukhovaksenya@users.noreply.github.com>
2022-11-14 14:48:44 +0300

    Add files via upload

---
+++ modulesLesson/disciplines.js
@@ -0,0 +1,20 @@
+let languages = [
+    {
+        discipline: 'Design',
+        teacher: 'Татьяна'
+    },
+    {
+        discipline: 'HTML',
+        teacher: 'Максим'
+    },
+    {
+        discipline: 'JS',
+        teacher: 'Халид'
+    },
+    {
+        discipline: 'PHP',
+        teacher: 'Неизвестен'
+    }
+]
+
+module.exports = languages;
